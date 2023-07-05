d262a9a85fe90b25284af0bb48d94782a5e3a455
Yash <yash20008@gmail.com>
2023-07-05 19:33:22 +0530

    Removed trailing brackets

--- src/components/home/EpisodeDetails.js
+++ src/components/home/EpisodeDetails.js
@@ -81,7 +81,7 @@ const EpisodeDetails = ({ data }) => {
       const response = await fetch(url);
       if (!response.ok) {
         throw new Error("Network response was not ok");
-      }]
+      }
      
       const data = await response.json();
     
